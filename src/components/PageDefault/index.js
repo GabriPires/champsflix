6cba4c16cd303b5f45a3784306210b03775c0f6d
GabriPires <gabriel.santos.pes@gmail.com>
2020-07-29 09:00:11 -0300

    Main no vídeo

--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -1,14 +1,26 @@
 import React from 'react';
 
+import styled from 'styled-components';
+
 import Menu from '../Menu';
 import Footer from '../Footer';
 
+const Main = styled.main`
+  background: var(--black);
+  color: var(--white);
+  flex: 1;
+  padding-top: 50px;
+  padding-left: 5%;
+  padding-top: 5%;
+`;
+
 // import { Container } from './styles';
 
-function PageDefault() {
+function PageDefault({ children }) {
   return (
     <>
       <Menu />
+      <Main>{children}</Main>
       <Footer />
     </>
   );
